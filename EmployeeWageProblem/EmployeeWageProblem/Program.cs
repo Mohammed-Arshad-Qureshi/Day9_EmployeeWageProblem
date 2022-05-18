a18322327e26faa4c9c8ea4599bbbce90d6e5f50
Arshad <mohammedarshad183@gmail.com>
2022-05-18 15:15:10 +0530

    Refactor: Calculate Employee wage Using switch

--- EmployeeWageProblem/EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/EmployeeWageProblem/Program.cs
@@ -8,26 +8,23 @@ namespace EmployeeWageProblem
 {
     class Program
     {
+        public const int IS_Full_Time = 1, IS_Part_Time = 2, Emp_Rate_Per_Hr = 20;
+
         static void Main(string[] args)
         {
             //Constants
-            int IS_Full_Time = 1, IS_Part_Time = 2, Emp_Rate_Per_Hr = 20;
             //varibles
             int empHrs = 0 ,empWage = 0;
             Random r = new Random();
             int empCheck = r.Next(0, 2);
-
-            if (empCheck == IS_Full_Time)
+            switch(empCheck)
             {
-                empHrs = 8;
-            }
-            else if(empCheck == IS_Part_Time)
-            {
-                empHrs = 4; 
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_Full_Time: empHrs = 8;
+                    break;
+                case IS_Part_Time: empHrs = 4;
+                    break;
+                default: empHrs = 0;
+                    break;
             }
             empWage = empHrs + Emp_Rate_Per_Hr;
             Console.WriteLine("Employee Wage : " + empWage);
