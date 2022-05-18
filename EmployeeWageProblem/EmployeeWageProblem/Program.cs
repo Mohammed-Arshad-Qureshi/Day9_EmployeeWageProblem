4fe0cff51ddb6e2103c2fa05050bcedbbcb2830f
Arshad <mohammedarshad183@gmail.com>
2022-05-18 15:54:10 +0530

    Calculating Wages till Number of Working Days or Total Working Hours per month is Reached

--- EmployeeWageProblem/EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/EmployeeWageProblem/Program.cs
@@ -8,17 +8,18 @@ namespace EmployeeWageProblem
 {
     class Program
     {
-        public const int IS_Full_Time = 1, IS_Part_Time = 2, Emp_Rate_Per_Hr = 20, Num_Of_Working_Days=2;
+        public const int IS_Full_Time = 1, IS_Part_Time = 2, Emp_Rate_Per_Hrs = 20, Num_Of_Working_Days = 20;
+        public const int Max_Hrs_In_Month = 100;
 
         static void Main(string[] args)
         {
             //Constants
             //varibles
-            int empHrs = 0, empWage = 0, totalEmpWage = 0;
-            for (int day = 0; day < Num_Of_Working_Days; day++)
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= Max_Hrs_In_Month && totalWorkingDays < Num_Of_Working_Days)
             {
                 Random r = new Random();
-                int empCheck = r.Next(0, 2);
+                int empCheck = r.Next(0, 3);
                 switch (empCheck)
                 {
                     case IS_Full_Time:
@@ -31,10 +32,11 @@ namespace EmployeeWageProblem
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs + Emp_Rate_Per_Hr;
-                totalEmpWage = totalEmpWage + empWage;
-                Console.WriteLine("Employee Wage : " + empWage);
+                totalEmpHrs = totalEmpHrs + empHrs;
+                Console.WriteLine("Day# :" + totalWorkingDays + "Emp Hrs :  " + empHrs);
+                totalWorkingDays++;
             }
+            int totalEmpWage = totalEmpHrs * Emp_Rate_Per_Hrs;
             Console.WriteLine("Total Employee Wage : " + totalEmpWage);
 
             Console.ReadLine();
